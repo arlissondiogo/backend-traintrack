d096b516b2ee44aaadfc843bedc5d84282f35515
arlissondiogo <arlisson.silva@aluno.uepb.edu.br>
2025-05-27 12:28:33 -0300

    feat: adiciona algumas formas de manipulação dos dados dos treinos (ainda deve ser melhorado para deixar mais intuitivo)

--- routes/progressaoRoutes.js
+++ routes/progressaoRoutes.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const router = express.Router();
-const { getProgressao } = require("../controllers/progressaoController");
-const authMiddleware = require("../middleware/authMiddleware.js");
+const authMiddleware = require("../middleware/authMiddleware");
 
-router.get("/progressao/:exercicio", authMiddleware, getProgressao);
+const {
+  getFrequenciaTreino,
+  getVolumeTotal,
+  getProgressoCarga,
+  getTempoTotal,
+  getDistribuicaoExercicios,
+  getComparativoExercicios,
+} = require("../controllers/progressaoController");
+
+router.get("/frequencia", authMiddleware, getFrequenciaTreino); // n° de treinos por semana/mês
+router.get("/volume-total", authMiddleware, getVolumeTotal); // volume total (series * rep * carga)
+router.get("/progresso-carga", authMiddleware, getProgressoCarga); // média ou pico de carga por exercício
+router.get("/tempo-treino", authMiddleware, getTempoTotal); // tempo total do treino
+router.get(
+  "/distribuicao-exercicios",
+  authMiddleware,
+  getDistribuicaoExercicios
+); // uso por nomeExercicio
+router.get("/comparativo-exercicios", authMiddleware, getComparativoExercicios); // comparação de cargas
 
 module.exports = router;
