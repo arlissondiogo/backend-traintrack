54956ed5a8226911049f791581ce21108e9a8376
arlissondiogo <arlisson.silva@aluno.uepb.edu.br>
2025-06-03 09:37:03 -0300

    feat: adiciona rota para filtrar exercícios

--- routes/progressaoRoutes.js
+++ routes/progressaoRoutes.js
@@ -5,9 +5,11 @@ const authMiddleware = require("../middleware/authMiddleware");
 const {
   getVolumeTotal,
   getProgressoCarga,
+  getExerciciosUnicos,
 } = require("../controllers/progressaoController");
 
 router.get("/volume-total", authMiddleware, getVolumeTotal);
 router.get("/progresso-carga", authMiddleware, getProgressoCarga);
+router.get("/exercicios-unicos", authMiddleware, getExerciciosUnicos);
 
 module.exports = router;
