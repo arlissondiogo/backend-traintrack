0ff771366737741f3a764de141ff1cc771366c45
arlissondiogo <arlisson.silva@aluno.uepb.edu.br>
2025-06-08 15:44:41 -0300

    refactor (workoutRoutes.js): modificar termos para inglês

--- routes/workoutRoutes.js
+++ routes/workoutRoutes.js
@@ -2,17 +2,17 @@ const express = require("express");
 const authenticate = require("../middleware/authMiddleware");
 const router = express.Router();
 const {
-  addWorkout,
+  addExercise,
   listWorkouts,
-  deleteWorkout,
-  updateWorkout,
-  getExerciciosUnicos,
+  deleteExercise,
+  updateExercise,
+  getUniqueExercises,
 } = require("../controllers/workoutController.js");
 
-router.post("/add-workout", authenticate, addWorkout);
+router.post("/add-exercise", authenticate, addExercise);
 router.get("/list-workout", authenticate, listWorkouts);
-router.delete("/delete-workout/:id", authenticate, deleteWorkout);
-router.put("/update-workout/:id", authenticate, updateWorkout);
-router.get("/exercicios", authenticate, getExerciciosUnicos);
+router.delete("/delete-exercise/:id", authenticate, deleteExercise);
+router.put("/update-exercise/:id", authenticate, updateExercise);
+router.get("/exercise", authenticate, getUniqueExercises);
 
 module.exports = router;
